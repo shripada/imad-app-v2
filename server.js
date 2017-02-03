ea730b12120baba0c35db3fe135cc263c29bd1f2
shripada <shripada@robosoftin.com>
2017-02-03 17:17:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article one is requested and will be serverd from here')
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-one', function (req, res) {
