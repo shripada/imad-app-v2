c1bbcb897cc86f8979ab3e07003849d8d900aa52
shripada <shripada@robosoftin.com>
2017-02-04 15:23:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
    res.send(createTemplate(createTemplate(articles[articleName])));
 });
 
